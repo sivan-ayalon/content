1858467eabeebe47d41415da58dee70c98c2bed9
Ni-Knight <davrahami@paloaltonetworks.com>
2024-04-01 17:12:06 +0300

    IPv4 regex changes

--- Packs/CommonScripts/Scripts/UnEscapeIPs/UnEscapeIPs.js
+++ Packs/CommonScripts/Scripts/UnEscapeIPs/UnEscapeIPs.js
@@ -6,7 +6,10 @@ function format_ip_list(ip_list) {
     var len = ip_list.length;
     var formatted_ips = new Array(len);
     ip_list.forEach(function(the_ip, index) {
-        formatted_ips[index] = format_ip(the_ip.trim());
+        the_ip = the_ip.trim()
+        the_ip = isNaN(the_ip[0]) ? the_ip.slice(1,) : the_ip  // Will remove non number char from beginning of IPv4
+        the_ip = isNaN(the_ip[the_ip.length-1]) ? the_ip.slice(0,-1) : the_ip  // Will remove non number char from end of IPv4
+        formatted_ips[index] = format_ip(the_ip);
     });
     return formatted_ips;
 }
